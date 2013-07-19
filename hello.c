316f2a794bef727d6d86ee3ecffbc39e9e5e21c2
Johannes Krampf <johannes.krampf@hevs.ch>
2013-07-19 13:41:43 +0200

    Change control flow

--- hello.c
+++ hello.c
@@ -18,12 +18,20 @@
 
 static EFI_GUID gEfiGlobalVariableGuid = EFI_GLOBAL_VARIABLE;
 
+typedef struct registers registers;
+struct registers {
+	UINT32 eax;
+	UINT32 ebx;
+	UINT32 ecx;
+	UINT32 edx;
+};
+
 void Sleep(UINTN seconds_to_sleep) {
 	uefi_call_wrapper(BS->Stall, 1, seconds_to_sleep * 1000 * 1000);
 }	
 
 void cpuid(UINT32 *eax, UINT32 *ebx, UINT32 *ecx, UINT32 *edx) {
-        asm("cpuid;" :"+a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx));
+	asm("cpuid;" :"+a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx));
 }
 
 void rdmsr(UINT32 reg, UINT32 *upper, UINT32 *lower) {
@@ -34,119 +42,89 @@ void wrmsr(UINT32 reg, UINT32 upper, UINT32 lower) {
 	asm("wrmsr;": : "d" (upper), "a" (lower), "c" (reg));
 }
 
-
-void ints_to_string(CHAR8* buffer, UINT32* a, UINT32* b, UINT32* c, UINT32* d) {
-    UINT32* ints[] = {a, b, c, d, NULL};
-    UINT32** pos = ints;
-
-    while(*pos) {
-        CopyMem(buffer, (void*)*pos, 4);
-        buffer += 4;
-        pos++;
-    }
-}
-
-struct pvclock_wall_clock {
-	UINT32   version;
-	UINT32   sec;
-	UINT32   nsec;
-} __attribute__((__packed__));
-
-void print_time() {
-  struct pvclock_wall_clock clock;
-
-  wrmsr(0x4b564d00, (UINT32)((UINT64)&clock>>32 & 0xffffffff), (UINT32)((UINT64)&clock & 0xffffffff));
-  Print(L"Time: %d\n", clock.sec);
-}
-
-void print_vendor() {
-  UINT32 eax, ebx, ecx, edx;
-  CHAR8 vendor[15] = {0};
-
-  eax = 0;
-  cpuid(&eax, &ebx, &ecx, &edx);
-  ints_to_string(vendor, &ebx, &edx, &ecx, NULL);
-  Print(L"Vendor: %a\n", vendor);
-}
-
 void print_extended_vendor() {
-  UINT32 eax, ebx, ecx, edx;
-  CHAR8 vendor[50];
+	union {
+		registers regs;
+		UINT8 buffer[sizeof(registers)];
+	} data;
+	CHAR8 vendor[50];
 
-  eax = 0x80000002;
-  cpuid(&eax, &ebx, &ecx, &edx);
-  ints_to_string(vendor, &eax, &ebx, &ecx, &edx);
-  eax = 0x80000003;
-  cpuid(&eax, &ebx, &ecx, &edx);
-  ints_to_string(vendor+16, &eax, &ebx, &ecx, &edx);
-  eax = 0x80000004;
-  cpuid(&eax, &ebx, &ecx, &edx);
-  ints_to_string(vendor+32, &eax, &ebx, &ecx, &edx);
-  Print(L"Extended vendor: %a\n", vendor);
+	data.regs.eax = 0x80000002;
+	cpuid(&data.regs.eax, &data.regs.ebx, &data.regs.ecx, &data.regs.edx);
+	CopyMem(vendor, data.buffer, sizeof(data.buffer));
+	data.regs.eax = 0x80000003;
+	cpuid(&data.regs.eax, &data.regs.ebx, &data.regs.ecx, &data.regs.edx);
+	CopyMem(vendor + 16, data.buffer, sizeof(data.buffer));
+	data.regs.eax = 0x80000004;
+	cpuid(&data.regs.eax, &data.regs.ebx, &data.regs.ecx, &data.regs.edx);
+	CopyMem(vendor + 32, data.buffer, sizeof(data.buffer));
+	Print(L"Extended vendor: %a\n", vendor);
 }
 
-void boot_option(EFI_HANDLE ImageHandle, CHAR16* skipOption, UINT16 code) {
-	void* boot;
+BOOLEAN load_boot_option(EFI_HANDLE ImageHandle, CHAR16* skipOption, UINT16 code, EFI_HANDLE* image) {
 	CHAR16 VarBoot[16] = { 0 };
-	EFI_HANDLE image;
 	EFI_STATUS err;
+	void* boot;
 
 	CHAR16* Description;
 	EFI_DEVICE_PATH* path;
 
 	SPrint(VarBoot, sizeof(VarBoot), L"Boot%04x", code);
-  	boot = LibGetVariable(VarBoot, &gEfiGlobalVariableGuid);
+		boot = LibGetVariable(VarBoot, &gEfiGlobalVariableGuid);
 
 	Description = boot + sizeof(UINT32) + sizeof(UINT16);
 	
 	if (StrCmp(skipOption, Description) == 0) {
 		Print(L"Skipping Hello Chain loader.\n");
 		FreePool(boot);
-		return;
+		return FALSE;
 	}
 
 	path = (void*)Description + StrSize(Description);
 
 	Print(L"Description: %s\n", Description);
-	Sleep(3);
-  	Input(L"Press any key to boot this option.", NULL, 0);
-	err = uefi_call_wrapper(BS->LoadImage, 6, FALSE, ImageHandle, path , NULL, 0, &image);
+		Input(L"Press any key to boot this option.", NULL, 0);
+	err = uefi_call_wrapper(BS->LoadImage, 6, FALSE, ImageHandle, path , NULL, 0, image);
 
 	if (EFI_ERROR(err)) {
 		Print(L"Error loading %s: %r", Description, err);
 		FreePool(boot);
 		Sleep(3);
-		return;
-	}
-	
-	err = uefi_call_wrapper(BS->StartImage, 3, image, NULL, NULL);
-	if (EFI_ERROR(err)) {
-		Print(L"Error starting %s: %r", Description, err);
-		Sleep(3);
+		return FALSE;
 	}
 
 	FreePool(boot);
+	return TRUE;
 }
 
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-  UINT16 *boot_order;
-  UINT16 *pos;
-  UINTN size;
+	UINT16 *boot_order;
+	UINT16 *pos;
+	UINTN size;
+	EFI_HANDLE image;
+	EFI_STATUS err;
 
-  InitializeLib(ImageHandle, SystemTable);
+	InitializeLib(ImageHandle, SystemTable);
 
-  print_time();
-  print_vendor();
-  print_extended_vendor();
+	print_extended_vendor();
 
-  boot_order = LibGetVariableAndSize(VarBootOrder, &gEfiGlobalVariableGuid, &size);
-  for (pos = boot_order; pos < boot_order + size; pos++) {
-    boot_option(ImageHandle, L"Hello Chain loader", *pos);    
-  }
-  FreePool(boot_order);
+	boot_order = LibGetVariableAndSize(VarBootOrder, &gEfiGlobalVariableGuid, &size);
 
-  Input(L"Press any key to continue", NULL, 0);
+	for (pos = boot_order; pos < boot_order + size; pos++) {
+		if (load_boot_option(ImageHandle, L"Hello Chain loader", *pos, &image)) {		
+			FreePool(boot_order);
+			err = uefi_call_wrapper(BS->StartImage, 3, image, NULL, NULL);
+			if (EFI_ERROR(err)) {
+				Print(L"Error starting selected image: %r", err);
+				Sleep(3);
+			}
+		}
+	}
+
+	FreePool(boot_order);
+
+	Input(L"Press any key to continue", NULL, 0);
  
-  return EFI_SUCCESS;
+	return EFI_SUCCESS;
 }
